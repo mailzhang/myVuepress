0ac6596dbd17c2ffb33b1170ea2ec25262c31c09
nefulan <2426398674@qq.com>
2022-04-03 12:56:38 +0800

    modify nginx conf

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 docker pull nginx
 
-docker run --name myVuepress -p 80:80 -v nginx.conf:/usr/share/nginx/html -v docs/dist:/usr/share/nginx/html -d nginx
+docker run --name myVuepress -p 80:80 -v nginx.conf:/etc/nginx/nginx.conf -v docs/dist:/usr/share/nginx/html/dist -d nginx
